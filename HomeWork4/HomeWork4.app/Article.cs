de7616f7dd0b0439142c6058b26013740d7b9fc1
Bairamov Allaz <67458149+BairamovAllaz@users.noreply.github.com>
2022-05-24 01:16:45 +0300

    Some Changes

---
+++ HomeWork4/HomeWork4.app/Article.cs
@@ -0,0 +1,27 @@
+﻿namespace HomeWork4.app;
+
+public enum ProductEnum
+{
+    Phone,
+    NoteBook,
+    Food, 
+    Clothes, 
+    Car
+}
+
+public class Article
+{
+    private uint _ProductCode;
+    private ProductEnum _NameProduct;
+    private double _PriceProduct;
+    public Article(uint productCode, ProductEnum nameProduct, double priceProduct)
+    {
+        _ProductCode = productCode;
+        _NameProduct = nameProduct;
+        _PriceProduct = priceProduct;   
+    }
+    public override string ToString()
+    {
+        return $"Code : {_ProductCode} Name: {_NameProduct} Price: {_PriceProduct}";
+    }
+}
---
+++ HomeWork4/HomeWork4.app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeWork4.app 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Article article = new Article(1234, ProductEnum.Clothes, 100000);
+            Console.WriteLine(article);
+        }
+    }
+}
