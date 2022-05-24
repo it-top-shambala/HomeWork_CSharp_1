da2cee73b9a36d4148f7e0afe84c0907262ff3e2
Bairamov Allaz <67458149+BairamovAllaz@users.noreply.github.com>
2022-05-24 23:48:03 +0300

    Add files via upload

---
+++ HomeWork7/HomeWork7.app/Request.cs
@@ -0,0 +1,40 @@
+﻿namespace HomeWork7.app;
+
+
+public enum EnumPayType
+{
+    NFC, 
+    Online, 
+    Application,
+    Cash
+}
+
+public class Request
+{
+    public int CodeRequest { get; set; }
+    public string Client { get; set; }
+    private DateTime _dateRequest;
+    public string OrderName { get; set; }
+    public uint PriceOrder { get; set; }
+    public EnumPayType PayType { get; set; }
+    public DateTime DateTime
+    {
+        get => _dateRequest;
+        set
+        {
+            if (value > DateTime.Now)
+            {
+                throw new TimeoutException();
+            }
+            _dateRequest = value;
+        }
+    }
+    public override string ToString()
+    {
+        return
+            $"Coderequest: {CodeRequest} Client: {Client}" +
+            $" PayType : {PayType}" +
+            $" OrderName: {OrderName} PricerOrder: {PriceOrder} " +
+            $" Date: {DateTime} "; 
+    }
+}
