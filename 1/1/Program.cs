a30661fb9b5b63f234b3037cc24aa7b37ded3a59
emoclew33 <92328680+emoclew33@users.noreply.github.com>
2022-05-24 23:02:00 +0600

    Add files via upload

---
+++ 1/1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Square;
+
+Console.WriteLine("Введите A, B прямоугольника через пробел");
+string line = Console.ReadLine();
+string[] splitLine = line.Split(' ');
+int a = int.Parse(splitLine[0]);
+int b = int.Parse(splitLine[1]);
+
+Console.WriteLine("Введите размер квадрата");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SquareCount.Search(a, b, c));
+
---
+++ 2/2/Bank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    public class Bank
+    {
+        private int _deposit;
+        public int Deposit { get => _deposit; }
+
+        private int _p;
+        public int P {
+            get => _p;
+            set
+            {
+                _p = value > 25 ? 25 : value < 0 ? 0 : value;
+            }
+        }
+        public Bank() { }
+        public Bank(int p = 0)
+        {
+            _p = p;
+            _deposit = 10000;
+        }
+        public int Count()
+        {
+            double tempP = (double)_p / 100;
+            int i = 0;
+            while(_deposit < 11000)
+            {
+                i++;
+                _deposit = _deposit + (int)(_deposit * tempP);
+            }
+            return i;
+        }
+    }
+}
