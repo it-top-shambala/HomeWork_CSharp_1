d3cc05b10aca78cb4a1168e32cfa722f47e87b9e
mytnii <83296383+mytnii@users.noreply.github.com>
2022-05-25 15:35:53 +0300

    Add files via upload
    чтото мне подсказывает что я много лишнего в коде написал и скинул на проверку лишка

---
+++ Classes/RequestItem.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Classes
+{
+    public class RequestItem
+    {
+        private string? _product;
+        public int _productQuantity;
+
+       public  static int ProductQuantity
+        {
+            get;
+        }
+
+        public RequestItem(string product, int productQuantity)
+        {
+            this._product = product;
+            this._productQuantity = productQuantity;
+        }
+
+        public void PrintRequestItem()
+        {
+            Console.WriteLine($"{this._product, 15} {this._productQuantity, 10}\n");
+        }
+    }
+}
