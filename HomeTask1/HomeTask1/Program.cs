f100229815a2b689412d1e86b68c6ac3008deb7e
Anatoli Erokhin <gemusenyou@yandex.ru>
2022-05-26 16:42:01 +0300

    Add files via upload

---
+++ HomeTask1/HomeTask1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeTask1
+{
+    class Program
+    {
+        private static int rectangle(int A, int B)
+        {
+            return A * B;
+        }
+        static void Main(string[] args)
+        {
+            int A, B, C;
+            Console.Write("Введите А: ");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nВведите B: ");
+            B = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nВведите C: ");
+            C = Convert.ToInt32(Console.ReadLine());
+
+            if (C >= A || C >= B)
+                Console.WriteLine("\'С\' не может быть меньше любой из сторон.");
+            else
+            {
+                Console.WriteLine($"Количество квадратов: {rectangle(A, B) / rectangle(C, C)}");
+                Console.WriteLine($"Не занятая часть прямоугольника: {rectangle(A, B) % rectangle(C, C)}");
+            }
+        }
+    }
+}
---
+++ HomeTask2/HomeTask2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cont = 10000;
+            double P, S;
+            int K;
+            Console.WriteLine("Введите процент:");
+            P = Convert.ToInt32(Console.ReadLine());
+            if (P < 0 || P > 25)
+            {
+                Console.WriteLine("Введен неверный процент.");
+            }
+            else
+            {
+                K = (int)(1000 / (100 * P));
+                if ((cont + 100 * P * K) < 11000)
+                    K++;
+                S = cont + 100 * P * K;
+                Console.WriteLine($"Размер вклада увеличится до 11000 через {K} месяцев.");
+                Console.WriteLine($"Итоговый размер вклада {S}.");
+            }
+
+
+        }
+    }
+}
