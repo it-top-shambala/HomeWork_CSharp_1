41fe9e1afb7a97cb08da0727550147f3afa702fc
emoclew33 <92328680+emoclew33@users.noreply.github.com>
2022-05-26 20:18:41 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите А и Б через пробел");
+string line = Console.ReadLine();
+string[] splitLine = line.Split(' ');
+int a = int.Parse(splitLine[0]);
+int b = int.Parse(splitLine[1]);
+
+for(int i = a; i <= b; i++)
+{
+    for(int j = 0; j < i; j++)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+}
