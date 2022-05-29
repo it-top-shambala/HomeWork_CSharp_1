f69be3b43ce1918c433d0ed59e2e5e824b8af19b
Elena-Miro <73115987+Elena-Miro@users.noreply.github.com>
2022-05-29 19:57:27 +0500

    Елена Мироненко
    Article

---
+++ Article/Article/RequestItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Article
+{
+    class RequestItem
+    {
+        string product;
+        public int quantity;
+
+        public RequestItem
+            (
+            string product, int quantity
+            )
+        {
+            this.product = product;
+            this.quantity = quantity;
+        }
+
+        public void PrintRequestItem()
+        {
+            Console.WriteLine($"Товар: {product}");
+            Console.WriteLine($"Количество единиц товара: {quantity}");
+        }
+    }
+}
